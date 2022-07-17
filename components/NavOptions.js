57a157db6c902e4d47099bed0613d7036df0d2bf
zwyeo <>
2022-07-18 00:39:39 +0800

    ui initalised

---
+++ components/NavOptions.js
@@ -0,0 +1,54 @@
+import { FlatList, Image, Text, TouchableOpacity, View } from "react-native";
+import React from "react";
+import tw from "twrnc";
+import { Icon } from "react-native-elements";
+import { useNavigation } from "@react-navigation/native";
+
+const data = [
+  {
+    id: "123",
+    title: "Get a ride",
+    image: "https://links.papareact.com/3pn",
+    screen: "MapScreen",
+  },
+  {
+    id: "456",
+    title: "Order food",
+    image: "https://links.papareact.com/28w",
+    screen: "EatsScreen", //? to be inplement in the future
+  },
+];
+
+const NavOptions = () => {
+  const navigation = useNavigation();
+
+  return (
+    <FlatList
+      data={data}
+      keyExtractor={(item) => item.id}
+      horizontal
+      renderItem={({ item }) => (
+        <TouchableOpacity
+          onPress={() => navigation.navigate(item.screen)}
+          style={tw`p-2 pl-6 pb-8 pt-4 bg-gray-200 m-2 w-40`}
+        >
+          <View>
+            <Image
+              style={{ width: 120, height: 120, resizeMode: "contain" }}
+              source={{ uri: item.image }}
+            />
+            <Text style={tw`mt-2 text-lg font-semibold`}>{item.title}</Text>
+            <Icon
+              style={tw`p-2 bg-black rounded-full w-10 mt-4`}
+              type="antdesign"
+              name="arrowright"
+              color="white"
+            />
+          </View>
+        </TouchableOpacity>
+      )}
+    />
+  );
+};
+
+export default NavOptions;
---
+++ slices/navSlice.js
@@ -0,0 +1,34 @@
+import { createSlice } from "@reduxjs/toolkit";
+
+const initialState = {
+  origin: null,
+  destination: null,
+  travelTimeInfomation: null,
+};
+
+export const navSlice = createSlice({
+  name: "nav",
+  initialState: initialState,
+  reducers: {
+    setOrigin: (state, action) => {
+      state.origin = action.payload;
+    },
+    setDestination: (state, action) => {
+      state.destination = action.payload;
+    },
+    setTravelTimeInformation: (state, action) => {
+      state.travelTimeInformation = action.payload;
+    },
+  },
+});
+
+export const { setOrigin, setDestination, setTravelTimeInformation } =
+  navSlice.actions;
+
+// Selectors
+export const selectOrigin = (state) => state.nav.origin;
+export const selectDestination = (state) => state.nav.destination;
+export const selectTravelTimeInformation = (state) =>
+  state.nav.travelTimeInfomation;
+
+export default navSlice.reducer;
